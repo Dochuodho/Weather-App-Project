21a9e3fffe9e672e0d6adc45119a4aeeb8b97517
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-01 16:21:50 +0300

    Updating data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let apiKey= "0da1b2c6148eea77db878584a3f04e72";
 let apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi";
-
+//displaying in JSON format
 async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
@@ -8,4 +8,9 @@ async function checkWeather(){
 
     console.log(data);
 
+    //Updating our values according to data that is coming from the API
+
+
 }
+
+checkWeather()
