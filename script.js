8aece55d44f831b4602b83d966d891bcdd4524ac
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-01 16:46:01 +0300

    Different images for different weather

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let apiKey= "0da1b2c6148eea77db878584a3f04e72";
-let apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi";
+let apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 //displaying in JSON format
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
 
@@ -11,10 +15,30 @@ async function checkWeather(){
     //Updating our values according to data that is coming from the API
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    
+    
 
 }
 
-checkWeather()
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
