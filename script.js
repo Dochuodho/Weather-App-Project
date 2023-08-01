6ab6b427883a8b62f64916a325a1150514554055
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-01 16:17:14 +0300

    Asynchronous function

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let apiKey= "0da1b2c6148eea77db878584a3f04e72";
 let apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+
+    console.log(data);
+
+}
