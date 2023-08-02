26d04488f6c095f6f2629f728c69bdf979c46b3c
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-02 12:15:59 +0300

    Completed code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+const boxSearch = document.querySelector(".searchCity input");
+const buttonSearch = document.querySelector(".searchCity button");
+const icon = document.querySelector(".icon")
+
+//Declaring the API key and API Url
 let apiKey= "0da1b2c6148eea77db878584a3f04e72";
 let apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
-const searchBox = document.querySelector(".searchCity input");
-const searchBtn = document.querySelector(".searchCity button");
-const weatherIcon = document.querySelector(".icon")
 //displaying the data in JSON format
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -15,31 +17,29 @@ async function checkWeather(city){
     }else{
     var data = await response.json();
 
-
     console.log(data);
 
     //Updating our values according to data that is coming from the API
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";//To roundoff the temperature
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";//To roundoff the temperature
 
     if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png";
+        icon.src = "images/drizzle.png";
     }
     else if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
+        icon.src = "images/clouds.png";
     }
     
     else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "images/mist.png";
+        icon.src = "images/mist.png";
     }
     else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png";
+        icon.src = "images/rain.png";
     }
     else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "images/clear.png";
+        icon.src = "images/clear.png";
     }
 
     document.querySelector(".weather").style.display = "block";
@@ -51,6 +51,6 @@ async function checkWeather(city){
    
 }
 
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
+buttonSearch.addEventListener("click", ()=>{
+    checkWeather(boxSearch.value);
 })
